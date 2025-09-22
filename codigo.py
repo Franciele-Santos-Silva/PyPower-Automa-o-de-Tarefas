be2a0afe87fd7247779c17b7cd37a70c0e567530
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-22 00:32:51 -0300

    Testando pyautogui

--- codigo.py
+++ codigo.py
@@ -1,3 +1,25 @@
-#Passo 1: Entrar no sistema da empresa - "https://dlp.hashtagtreinamentos.com/python/intensivao/login
-#Passo 2: Fazer login
-#Passo 3: Importar a base de produtos pra cadastrar
+import pyautogui 
+#Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login .
+#abrir o navegador (chrome) .
+pyautogui.press('win')
+#digitar o site
+pyautogui.write('chrome')
+
+
+
+
+
+
+
+
+#Passo 2: Fazer login .
+#Passo 3: Importar a base de dados de produtos pra cadastrar .
+#Passo 4: Cadastrar um produto .
+#Passo 5: Repetir o cadastro para todos os produtos .
+
+#pyautogui -> faz automaçoes em python(controla mouse,teclado e tela do computador)
+
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.write -> escrever um texto
+# pyautogui.hotkey -> combinação de teclas 
